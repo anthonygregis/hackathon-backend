0ba11e4f3a92fe9c90005553ef0f9adacfe716da
Anthony Gregis <anthony@thedevtron.com>
2020-09-23 20:33:26 -0500

    [Clinics] Proper returns for empty clinics, no urls, and bad & symbols

--- routes/clinics.js
+++ routes/clinics.js
@@ -7,20 +7,29 @@ const mapbox_token = process.env.MAPBOX_SECRET
 /* GET Clinics off zipcode */
 router.get('/zipcode/:zipcode', function(req, res, next) {
   let zipcode = req.params.zipcode
-  let distance = parseInt(req.query.distance)
+  let radius = parseInt(req.query.radius)
   const requestParams = `https://api.mapbox.com/geocoding/v5/mapbox.places/${zipcode}.json?access_token=${mapbox_token}&country=us&types=postcode&limit=1`
   axios.get(requestParams)
     .then(results => {
-        if (results.data.features[0].geometry) {
+        if (results.data.features[0]) {
             let lon = results.data.features[0].geometry.coordinates[0]
             let lat = results.data.features[0].geometry.coordinates[1]
-            axios.get(`https://findahealthcenter.hrsa.gov//healthcenters/find?lon=${lon}&lat=${lat}&radius=${distance}`)
+            axios.get(`https://findahealthcenter.hrsa.gov//healthcenters/find?lon=${lon}&lat=${lat}&radius=${radius}`)
                 .then(results => {
-                    let cleanedClinics = results.data.map(({Shape, DwRecordCreateDt, ApproxValueCd, LocNameDesc, EndDt, ParentCtrCity, FilteredResultsReturned, ...keepTheseFields }) => keepTheseFields )
+                    let cleanedClinics = results.data.map(({Shape, DwRecordCreateDt, ApproxValueCd, LocNameDesc, EndDt, ParentCtrCity, FilteredResultsReturned, SiteUrl, CtrNm, ...keepTheseFields }) => {
+                        if (SiteUrl !== "" && !SiteUrl.includes("http")) {
+                            SiteUrl = "http://" + SiteUrl
+                        }
+                        CtrNm = CtrNm.replace("&amp;", "&")
+                        return {CtrNm, SiteUrl, ...keepTheseFields}
+                    })
                     let filteredClinics = cleanedClinics.filter(clinic => clinic.Covid19TestStatus == "Yes" || clinic.TeleHealthStatus == "Yes")
-                    res.send(filteredClinics)
+
+                    res.send(filteredClinics)                
                 })
                 .catch(err => console.log(err))
+        } else {
+            res.status(400).send("Incorrect zipcode format, please try again.")
         }
     })
     .catch(err => console.log(err))
@@ -29,14 +38,14 @@ router.get('/zipcode/:zipcode', function(req, res, next) {
 /* GET Clinics off Address */
 router.get('/address', function(req, res, next) {
     let address = encodeURI(req.query.input)
-    let distance = parseInt(req.query.distance)
+    let radius = parseInt(req.query.radius)
     const requestParams = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${mapbox_token}&country=us&types=address&limit=1`
     axios.get(requestParams)
         .then(results => {
             if (results.data.features[0].geometry) {
                 let lon = results.data.features[0].geometry.coordinates[0]
                 let lat = results.data.features[0].geometry.coordinates[1]
-                axios.get(`https://findahealthcenter.hrsa.gov//healthcenters/find?lon=${lon}&lat=${lat}&radius=${distance}`)
+                axios.get(`https://findahealthcenter.hrsa.gov//healthcenters/find?lon=${lon}&lat=${lat}&radius=${radius}`)
                     .then(results => {
                         let cleanedClinics = results.data.map(({Shape, DwRecordCreateDt, ApproxValueCd, LocNameDesc, EndDt, ParentCtrCity, FilteredResultsReturned, ...keepTheseFields }) => keepTheseFields )
                         let filteredClinics = cleanedClinics.filter(clinic => clinic.Covid19TestStatus == "Yes" || clinic.TeleHealthStatus == "Yes")

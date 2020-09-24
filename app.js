9d823d0a3e8dd3e17c2772f63aa1713f5bdf8dbd
Anthony Gregis <anthony@thedevtron.com>
2020-09-24 11:21:12 -0500

    [Production] Prepare backend for production

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-var createError = require('http-errors');
 var express = require('express');
 var cors = require('cors')
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -24,15 +22,8 @@ app.use(function(req, res, next) {
   res.status(404).send("No Route Found")
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen("4000", () => {
+  console.log("Server running on port 4000")
+})
 
 module.exports = app;
